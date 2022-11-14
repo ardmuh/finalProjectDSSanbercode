518c5dbb8ee6683641e82b41fe6f6a51a60ec97e
Ardi Muh <96427240+ardmuh@users.noreply.github.com>
2022-11-14 08:36:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import nltk
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
+
 
 st.title("Sentiment Analysis of TV Digital")
 
